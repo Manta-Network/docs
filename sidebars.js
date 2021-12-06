01089d39d65fa548ea5ec6519e98982fb5737997
Shumo Chu <shumo.chu@protonmail.com>
2021-12-05 21:43:23 -0800

    fix build

--- sidebars.js
+++ sidebars.js
@@ -31,7 +31,7 @@ module.exports = {
         //'calamari/MariSwap',
         'calamari/Governance',
         'calamari/API',
-        'calamari/RunNode',
+        //'calamari/RunNode',
         'calamari/CrowdloanClaim' 
       ],
     },
