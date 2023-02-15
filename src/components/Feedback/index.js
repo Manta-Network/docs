56583dcf6af546b884789b3de6c397ea4777f1bd
chan <jincan39@163.com>
2023-02-15 11:16:17 +0800

    style: update themed components

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import { useColorMode } from "@docusaurus/theme-common";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 
 const DocsRating = ({ label }) => {
+    const { colorMode, setColorMode } = useColorMode();
+    const helpfulString =
+        label === "top" ? "Helpful" : "Was this page helpful?";
     if (!ExecutionEnvironment.canUseDOM) {
         return null;
     }
@@ -23,28 +27,51 @@ const DocsRating = ({ label }) => {
     return (
         <div className={styles.content}>
             <div className={styles.innerContent}>
-                <div className={styles.font}>
+                <div
+                    className={styles.font}
+                    style={{
+                        color:
+                            colorMode === "dark"
+                                ? "rgba(255, 255, 255, 0.6)"
+                                : "rgba(0, 0, 0, 0.6)",
+                    }}
+                >
                     {vote !== undefined
                         ? "Thanks for letting us know!"
-                        : "Was this page helpful?"}
+                        : helpfulString}
                 </div>
                 {vote === undefined || vote === 1 ? (
                     <img
-                        onClick={() => giveFeedback(1)}
+                        style={{
+                            cursor: vote === undefined ? "pointer" : "unset",
+                            backgroundColor: "#97E910",
+                            borderRadius: 16,
+                        }}
+                        onClick={() => !vote && giveFeedback(1)}
                         height={16}
                         src="/img/face-smile-regular.svg"
                     />
                 ) : null}
                 {vote === undefined || vote === 0 ? (
                     <img
-                        onClick={() => giveFeedback(0)}
+                        style={{
+                            cursor: vote === undefined ? "pointer" : "unset",
+                            backgroundColor: "#FFE606",
+                            borderRadius: 16,
+                        }}
+                        onClick={() => !vote && giveFeedback(0)}
                         height={16}
                         src="/img/face-meh-regular.svg"
                     />
                 ) : null}
                 {vote === undefined || vote === -1 ? (
                     <img
-                        onClick={() => giveFeedback(-1)}
+                        style={{
+                            cursor: vote === undefined ? "pointer" : "unset",
+                            backgroundColor: "#FF431A",
+                            borderRadius: 16,
+                        }}
+                        onClick={() => !vote && giveFeedback(-1)}
                         height={16}
                         src="/img/face-frown-regular.svg"
                     />
