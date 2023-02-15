c3d14a42ac009867b057b8a50b937ed8eb0a06b9
chan <jincan39@163.com>
2023-02-15 17:27:33 +0800

    ..

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -2,12 +2,19 @@ import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import { useColorMode } from "@docusaurus/theme-common";
 import clsx from "clsx";
 import React, { useState } from "react";
+import Bad from "./Bad";
+import Good from "./Good";
+import Normal from "./Normal";
 import styles from "./styles.module.css";
 
 const RatingEnum = { GOOD: 1, NORMAL: 0, BAD: -1 };
 
 const DocsRating = ({ label }) => {
     const { colorMode, setColorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+    const isLight = colorMode === "light";
+    const darkColor = "rgba(255,255,255,0.6)";
+    const lightColor = "rgba(0,0,0,0.6)";
     const isTop = label === "top";
     const helpfulString = isTop ? "Helpful" : "Was this page helpful?";
     if (!ExecutionEnvironment.canUseDOM) {
@@ -31,6 +38,9 @@ const DocsRating = ({ label }) => {
     return (
         <div className={styles.content}>
             <div
+                style={{
+                    backgroundColor: isDark ? "rgba(255, 255, 255, 0.05)" : "",
+                }}
                 className={clsx(
                     styles.innerContent,
                     isTop ? styles.topContainer : null
@@ -39,53 +49,49 @@ const DocsRating = ({ label }) => {
                 <div
                     className={styles.font}
                     style={{
-                        color:
-                            colorMode === "dark"
-                                ? "rgba(255, 255, 255, 0.6)"
-                                : "rgba(0, 0, 0, 0.6)",
+                        color: isDark
+                            ? "rgba(255, 255, 255, 0.6)"
+                            : "rgba(0, 0, 0, 0.6)",
                     }}
                 >
                     {unVoted ? helpfulString : "Thanks for letting us know!"}
                 </div>
                 {unVoted || vote === RatingEnum.GOOD ? (
-                    <img
+                    <Good
                         style={{
                             cursor: unVoted ? "pointer" : "unset",
                             backgroundColor:
                                 vote === RatingEnum.GOOD ? "#97E910" : "",
                             borderRadius: 16,
                         }}
+                        fill={isLight || !unVoted ? lightColor : darkColor}
                         onClick={() => unVoted && giveFeedback(RatingEnum.GOOD)}
-                        height={16}
-                        src="/img/face-smile-regular.svg"
                     />
                 ) : null}
                 {unVoted || vote === RatingEnum.NORMAL ? (
-                    <img
+                    <Normal
                         style={{
                             cursor: unVoted ? "pointer" : "unset",
                             backgroundColor:
                                 vote === RatingEnum.NORMAL ? "#FFE606" : "",
                             borderRadius: 16,
                         }}
+                        fill={isLight || !unVoted ? lightColor : darkColor}
                         onClick={() =>
                             unVoted && giveFeedback(RatingEnum.NORMAL)
                         }
-                        height={16}
-                        src="/img/face-meh-regular.svg"
                     />
                 ) : null}
                 {unVoted || vote === RatingEnum.BAD ? (
-                    <img
+                    <Bad
                         style={{
                             cursor: unVoted ? "pointer" : "unset",
                             backgroundColor:
                                 vote === RatingEnum.BAD ? "#FF431A" : "",
                             borderRadius: 16,
                         }}
+                        fill={isLight || !unVoted ? lightColor : darkColor}
                         onClick={() => unVoted && giveFeedback(RatingEnum.BAD)}
-                        height={16}
-                        src="/img/face-frown-regular.svg"
                     />
                 ) : null}
             </div>
