6c8a555d7041f341baa0e883517d03cfe627a5ca
Chan J <jincan39@163.com>
2023-12-21 00:28:38 +0800

    chore: disable atlantic doc temp (#202)

--- sidebars.js
+++ sidebars.js
@@ -154,29 +154,33 @@ module.exports = {
                 },
             ],
         },
-        {
-            type: "category",
-            label: "Manta Atlantic Network",
-            collapsible: false,
-            items: [
-                {
-                    type: "category",
-                    label: "Collator",
-                    items: [
-                        {
-                            type: "doc",
-                            label: "Overview",
-                            id: "manta-atlantic/Collator Overview",
-                        },
-                        {
-                            type: "doc",
-                            label: "Requirement",
-                            id: "manta-atlantic/Collator Requirement",
-                        },
-                    ],
-                },
-            ],
-        },
+        // {
+        //     type: "category",
+        //     label: "Manta Atlantic Network",
+        //     collapsible: false,
+        //     items: [
+        //         {
+        //             type: "category",
+        //             label: "Collator",
+        //             items: [
+        //                 "manta-atlantic/Collation/Overview",
+        //                 "manta-atlantic/Collation/Requirements",
+        //                 {
+        //                     type: "category",
+        //                     label: "SetupAndRun",
+        //                     items: [
+        //                         "manta-atlantic/Collation/SetupAndRun/installation",
+        //                         "manta-atlantic/Collation/SetupAndRun/configuration",
+        //                         "manta-atlantic/Collation/SetupAndRun/running",
+        //                         "manta-atlantic/Collation/SetupAndRun/sync",
+        //                         "manta-atlantic/Collation/SetupAndRun/keys",
+        //                         "manta-atlantic/Collation/SetupAndRun/bond",
+        //                     ],
+        //                 },
+        //             ],
+        //         },
+        //     ],
+        // },
         {
             type: "category",
             label: "OpenZL",
