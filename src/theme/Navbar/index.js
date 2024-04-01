1c7026fcc128d453ca9e1f6bbd06222d125fde93
Tyler Sehr <tysehr37@gmail.com>
2024-04-01 15:24:53 -0700

    feat: updated docsbot (#260)
    Updated Cookbook Docsbot to use latest version of the package (@cookbookdev/docsbot@^4.5.0) instead of now deprecated `@cookbookdev/docusaurus-chefgpt`
    
    Co-authored-by: Anton Dosta <a.dosta@icloud.com>

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavbarLayout from '@theme/Navbar/Layout';
 import NavbarContent from '@theme/Navbar/Content';
-import ChefGPT from "@cookbookdev/docusaurus-chefgpt/theme/SearchBar";
+import AskCookbook from '@cookbookdev/docsbot/react';
+
 export default function Navbar() {
   return (
     <NavbarLayout>
       <NavbarContent />
-      <ChefGPT />
+      <AskCookbook apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWMxOGM1MzA1MjA1MDZmZmEwMDhjMmUiLCJpYXQiOjE3MDcxODMxODcsImV4cCI6MjAyMjc1OTE4N30.diwQUey2v9RLd7MWuzI4Uw4ayhLKaditIfa062Sc2YQ" />
     </NavbarLayout>
   );
 }
