54d8d90d40b4a4a8370bf23fef8cbd4d3bddebdb
Chan J <jincan39@163.com>
2024-05-31 14:48:14 +0800

    feat: manta pacific docs update (#267)
    * feat: manta pacific docs update (draft)
    
    * update: Security Partners
    
    * fix: build broken links
    
    * update: pacific docs
    
    * update: pacific docs
    
    * update: testnet info
    
    * ..
    
    * update: manta pacific replica
    
    * fix: build
    
    * fix: title font size
    
    * update: atlantic network info
    
    * chore: update replica node

---
+++ docs/manta-pacific/Build on Manta/Network Information.md
@@ -0,0 +1,29 @@
+# Network Information
+
+## Manta Pacific Mainnet
+
+| Name               | Value                                               |
+| ------------------ | --------------------------------------------------- |
+| Network Name       | Manta Pacific L2 Rollup                             |
+| Description        | The public mainnet for Manta Pacific.               |
+| Network URL        | https://pacific-info.manta.network/                 |
+| RPC URL            | https://pacific-rpc.manta.network/http              |
+| Additional RPC URL | https://manta-pacific.drpc.org                      |
+|                    | https://www.tencentcloud-rpc.com/v2/manta/manta-rpc |
+|                    | https://r1.pacific.manta.systems/http               |
+|                    | https://manta.nirvanalabs.xyz/mantapublic           |
+| Chain ID           | 169                                                 |
+| Currency Symbol    | ETH                                                 |
+| Websocket URL      | wss://pacific-rpc.manta.network/ws                  |
+
+## Manta Pacific Sepolia Testnet
+
+| Category        | Information                                                                                                       |
+| --------------- | ----------------------------------------------------------------------------------------------------------------- |
+| Network Name    | Manta Pacific Sepolia Testnet                                                                                     |
+| Network URL     | [https://pacific-info.sepolia-testnet.manta.network/](https://pacific-info.sepolia-testnet.manta.network/)        |
+| Explorer URL    | [https://pacific-explorer.sepolia-testnet.manta.network](https://pacific-explorer.sepolia-testnet.manta.network/) |
+| RPC URL         | https://pacific-rpc.sepolia-testnet.manta.network/http                                                            |
+| Websocket URL   | wss://pacific-rpc.sepolia-testnet.manta.network/ws                                                                |
+| Chain ID        | 3441006                                                                                                           |
+| Currency Symbol | ETH                                                                                                               |
--- sidebars.js
+++ sidebars.js
@@ -38,6 +38,11 @@ module.exports = {
                     ],
                 },
                 {
+                    type: "doc",
+                    label: "Manta Token",
+                    id: "mantaToken/mantaToken",
+                },
+                {
                     type: "category",
                     label: "Research",
                     items: [
@@ -76,14 +81,41 @@ module.exports = {
             collapsible: false,
             items: [
                 {
+                    type: "doc",
+                    label: "Network Information",
+                    id: "manta-pacific/Build on Manta/Network Information",
+                },
+                // {
+                //     type: "category",
+                //     label: "Network Info",
+                //     items: [
+                //         {
+                //             type: "doc",
+                //             label: "Quickstart",
+                //             id: "manta-pacific/Quickstart",
+                //         },
+
+                //         {
+                //             type: "doc",
+                //             label: "Block Explorer",
+                //             id: "manta-pacific/Block Explorer",
+                //         },
+                //         {
+                //             type: "doc",
+                //             label: "JSON-RPC Nodes",
+                //             id: "manta-pacific/JSON-RPC Nodes",
+                //         },
+                //         {
+                //             type: "doc",
+                //             label: "Testnet Info",
+                //             id: "manta-pacific/Testnet Info",
+                //         },
+                //     ],
+                // },
+                {
                     type: "category",
-                    label: "Network Info",
+                    label: "Building on Manta",
                     items: [
-                        {
-                            type: "doc",
-                            label: "Quickstart",
-                            id: "manta-pacific/Quickstart",
-                        },
                         {
                             type: "doc",
                             label: "EVM Compatibility",
@@ -91,30 +123,143 @@ module.exports = {
                         },
                         {
                             type: "doc",
-                            label: "Block Explorer",
-                            id: "manta-pacific/Block Explorer",
+                            label: "Manta Contracts",
+                            id: "manta-pacific/Build on Manta/Contracts",
                         },
                         {
                             type: "doc",
-                            label: "JSON-RPC Nodes",
-                            id: "manta-pacific/JSON-RPC Nodes",
+                            label: "Tokens",
+                            id: "manta-pacific/Build on Manta/Tokens",
                         },
                         {
                             type: "doc",
-                            label: "Testnet Info",
-                            id: "manta-pacific/Testnet Info",
+                            label: "Fees",
+                            id: "manta-pacific/Build on Manta/Fees",
+                        },
+                        {
+                            type: "doc",
+                            label: "Differences between Ethereum and Manta",
+                            id: "manta-pacific/Build on Manta/Differences between Ethereum and MantaNetwork",
                         },
                     ],
                 },
                 {
-                    type: "doc",
-                    label: "Manta Token",
-                    id: "mantaToken/mantaToken",
+                    type: "category",
+                    label: "Tools",
+                    items: [
+                        {
+                            type: "doc",
+                            label: "Node Providers",
+                            id: "manta-pacific/Tools/Node Providers",
+                        },
+                        {
+                            type: "doc",
+                            label: "Block Explorers",
+                            id: "manta-pacific/Tools/Block Explorers",
+                        },
+                        {
+                            type: "doc",
+                            label: "Network Faucets",
+                            id: "manta-pacific/Tools/Network Faucets",
+                        },
+                        {
+                            type: "doc",
+                            label: "Oracles",
+                            id: "manta-pacific/Tools/Oracles",
+                        },
+                        {
+                            type: "doc",
+                            label: "Data Indexers",
+                            id: "manta-pacific/Tools/Data Indexers",
+                        },
+                        {
+                            type: "doc",
+                            label: "Onramps",
+                            id: "manta-pacific/Tools/Onramps",
+                        },
+                        {
+                            type: "doc",
+                            label: "Security Partners",
+                            id: "manta-pacific/Tools/Security Partners",
+                        },
+                    ],
+                },
+                {
+                    type: "category",
+                    label: "Bridges",
+                    items: [
+                        {
+                            type: "doc",
+                            label: " Bridge $MANTA Token Between Manta Pacific and Manta Atlantic",
+                            id: "manta-pacific/Tools/Bridge/Bridge $MANTA Token Between Manta Pacific and Manta Atlantic",
+                        },
+                        {
+                            type: "category",
+                            label: "Native Bridge",
+                            items: [
+                                {
+                                    type: "doc",
+                                    label: "How to Use Native Bridge",
+                                    id: "manta-pacific/Tools/Bridge/Native Bridge/How to Use Native Bridge",
+                                },
+                                {
+                                    type: "doc",
+                                    label: "How to deploy Ethereum token on Manta",
+                                    id: "manta-pacific/Tools/Bridge/Native Bridge/How to deploy Ethereum token on Manta",
+                                },
+                                {
+                                    type: "doc",
+                                    label: "How to list token on Native Bridge",
+                                    id: "manta-pacific/Tools/Bridge/Native Bridge/How to list token on Native Bridge",
+                                },
+                                {
+                                    type: "doc",
+                                    label: "How to use JS SDK bridge token between Ethereum and Manta",
+                                    id: "manta-pacific/Tools/Bridge/Native Bridge/How to use JS SDK bridge token between Ethereum and Manta",
+                                },
+                                {
+                                    type: "doc",
+                                    label: "FAQ",
+                                    id: "manta-pacific/Tools/Bridge/Native Bridge/FAQ",
+                                },
+                            ],
+                        },
+                        {
+                            type: "doc",
+                            label: "Third party Bridge",
+                            id: "manta-pacific/Tools/Bridge/Third party Bridge",
+                        },
+                        {
+                            type: "doc",
+                            label: "Bridging Celestia‘s $TIA to Manta Pacific",
+                            id: "manta-pacific/Tools/Bridge/Bridging Celestia‘s $TIA to Manta Pacific",
+                        },
+                    ],
+                },
+                {
+                    type: "category",
+                    label: "Manta Pacific Node",
+                    items: [
+                        // {
+                        //     type: "doc",
+                        //     label: "Hardware conditions",
+                        //     id: "manta-pacific/Manta Pacific Node/Hardware conditions",
+                        // },
+                        {
+                            type: "doc",
+                            label: "How to run Replica Node",
+                            id: "manta-pacific/Manta Pacific Node/How to run Replica Node",
+                        },
+                        // {
+                        //     type: "doc",
+                        //     label: "Join Telegram to get node updates",
+                        //     id: "manta-pacific/Manta Pacific Node/Join Telegram to get node updates",
+                        // },
+                    ],
                 },
                 {
                     type: "category",
                     label: "Universal Circuits (Alpha): zkShuffle",
-
                     items: [
                         {
                             type: "doc",
@@ -157,45 +302,20 @@ module.exports = {
                     label: "Manta Pacific Roadmap",
                     id: "concepts/Roadmap",
                 },
-                {
-                    type: "category",
-                    label: "Bridge to Manta Pacific ",
-                    items: [
-                        {
-                            type: "autogenerated",
-                            dirName: "bridge-to-pacific",
-                        },
-                    ],
-                },
-                {
-                    type: "doc",
-                    label: "Manta Pacific Ecosystem",
-                    id: "concepts/Manta Pacific Ecosystem",
-                },
-                {
-                    type: "doc",
-                    label: "Manta Pacific Replica Node",
-                    id: "manta-pacific/Replica Node",
-                },
-                {
-                    type: "doc",
-                    label: "RedStone Oracles",
-                    id: "manta-pacific/RedStone Oracles",
-                },
-                {
-                    type: "doc",
-                    label: "Supra Oracles",
-                    id: "manta-pacific/Supra Oracles",
-                },
+                // {
+                //     type: "category",
+                //     label: "Bridge to Manta Pacific ",
+                //     items: [
+                //         {
+                //             type: "autogenerated",
+                //             dirName: "bridge-to-pacific",
+                //         },
+                //     ],
+                // },
                 {
                     type: "doc",
                     label: "SPACE ID Name Service and SPACE ID",
                     id: "manta-pacific/Space ID",
-                },
-                {
-                    type: "doc",
-                    label: "Security Partners",
-                    id: "manta-pacific/Security Partners",
                 },
             ],
         },
@@ -204,6 +324,11 @@ module.exports = {
             label: "Manta Atlantic Network",
             collapsible: false,
             items: [
+                {
+                    type: "doc",
+                    label: "Network Information",
+                    id: "manta-atlantic/Network Information",
+                },
                 {
                     type: "category",
                     label: "Collator",
